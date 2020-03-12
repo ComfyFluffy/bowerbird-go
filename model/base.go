1cecff5a02a19fbbdcca50b06cce50a0581ecdf6
WOo0W <24245520+WOo0W@users.noreply.github.com>
2020-03-13 01:05:31 +0800

    updating model

--- model/base.go
+++ model/base.go
@@ -6,119 +6,129 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// User defines the person
 type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,string"`
-	Rating   int                `bson:",omitempty" json:"rating,omitempty"`
-	Source   string             `bson:",omitempty" json:"source"`
-	SourceID string             `bson:",omitempty" json:"sourceID"`
+	ID       primitive.ObjectID     `bson:"_id,omitempty" json:"id,string"`
+	Rating   int                    `bson:"rating,omitempty" json:"rating,omitempty"`
+	Source   string                 `bson:"source,omitempty" json:"source"`
+	SourceID string                 `bson:"sourceID,omitempty" json:"sourceID"`
+	Extended map[string]interface{} `bson:"extended,omitempty" json:"extended"`
 
-	TagIDs []primitive.ObjectID `bson:",omitempty" json:"-"`
+	TagIDs []primitive.ObjectID `bson:"tagIDs,omitempty" json:"-"`
 
 	Tags []Tag `bson:"-" json:"tags"`
 }
 
-// DBCollection returns the name of mongodb collection.
+// DBCollection returns the name of mongodb collection
 func (User) DBCollection() string {
 	return "users"
 }
 
+// UserProfile defines user details
 type UserProfile struct {
 	ID       primitive.ObjectID     `bson:"_id,omitempty" json:"id,string"`
-	UserID   primitive.ObjectID     `bson:",omitempty" json:"-"`
-	Page     Page                   `bson:",omitempty" json:"page"`
-	Extended map[string]interface{} `bson:",omitempty" json:"extended"`
+	UserID   primitive.ObjectID     `bson:"userID,omitempty" json:"-"`
+	Page     *Page                  `bson:"page,omitempty" json:"page"`
+	Banner   *Media                 `bson:"banner,omitempty" json:"banner,omitempty"`
+	Extended map[string]interface{} `bson:"extended,omitempty" json:"extended"`
 }
 
-// DBCollection returns the name of mongodb collection.
+// DBCollection returns the name of mongodb collection
 func (UserProfile) DBCollection() string {
 	return "user_profile"
 }
 
+// Post defines user created content
 type Post struct {
-	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id,string"`
-	Rating          int                `bson:",omitempty" json:"rating,omitempty"`
-	Source          string             `bson:",omitempty" json:"source,omitempty"`
-	SourceID        string             `bson:",omitempty" json:"sourceID,omitempty"`
-	SourceInvisible bool               `json:"sourceInvisible"`
+	ID              primitive.ObjectID     `bson:"_id,omitempty" json:"id,string"`
+	Rating          int                    `bson:"rating,omitempty" json:"rating,omitempty"`
+	Source          string                 `bson:"source,omitempty" json:"source,omitempty"`
+	SourceID        string                 `bson:"sourceID,omitempty" json:"sourceID,omitempty"`
+	SourceInvisible bool                   `bson:"sourceInvisible" json:"sourceInvisible"`
+	Extended        map[string]interface{} `bson:"extended,omitempty" json:"extended"`
 
-	OwnerID primitive.ObjectID   `bson:",omitempty" json:"-"`
-	TagsIDs []primitive.ObjectID `bson:",omitempty" json:"-"`
+	OwnerID primitive.ObjectID   `bson:"ownerID,omitempty" json:"-"`
+	TagIDs  []primitive.ObjectID `bson:"tagIDs,omitempty" json:"-"`
 
 	Owner User  `bson:"-" json:"owner,omitempty"`
 	Tags  []Tag `bson:"-" json:"tags,omitempty"`
 }
 
-// DBCollection returns the name of mongodb collection.
+// DBCollection returns the name of mongodb collection
 func (Post) DBCollection() string {
 	return "posts"
 }
 
+// PostDetail defines the detail of Post
 type PostDetail struct {
 	ID       primitive.ObjectID     `bson:"_id,omitempty" json:"id,string"`
-	PostID   primitive.ObjectID     `bson:",omitempty" json:"-"`
-	Date     time.Time              `bson:",omitempty" json:"date,omitempty"`
-	Page     Page                   `bson:",omitempty" json:"page"`
-	Extended map[string]interface{} `bson:",omitempty" json:"extended"`
+	PostID   primitive.ObjectID     `bson:"postID,omitempty" json:"-"`
+	Date     time.Time              `bson:"date,omitempty" json:"date,omitempty"`
+	Page     *Page                  `bson:"page,omitempty" json:"page"`
+	Extended map[string]interface{} `bson:"extended,omitempty" json:"extended"`
 }
 
-// DBCollection returns the name of mongodb collection.
+// DBCollection returns the name of mongodb collection
 func (PostDetail) DBCollection() string {
 	return "post_details"
 }
 
+// Collection defines the collection of Post
 type Collection struct {
-	ID   primitive.ObjectID `bson:"_id,omitempty" json:"id,string"`
-	Name string             `json:"name"`
+	ID       primitive.ObjectID     `bson:"_id,omitempty" json:"id,string"`
+	Name     string                 `bson:"name,omitempty" json:"name"`
+	Extended map[string]interface{} `bson:"extended,omitempty" json:"extended"`
 
-	TagIDs  []primitive.ObjectID `bson:",omitempty" json:"-"`
-	PostIDs []primitive.ObjectID `bson:",omitempty" json:"-"`
+	TagIDs   []primitive.ObjectID `bson:"tagIDs,omitempty" json:"-"`
+	PostIDs  []primitive.ObjectID `bson:"postIDs,omitempty" json:"-"`
+	MediaIDs []primitive.ObjectID `bson:"mediaIDs,omitempty" json:"-"`
 
-	Tags  []Tag  `bson:"-" json:"tags"`
-	Posts []Post `bson:"-" json:"posts"`
+	Tags  []Tag   `bson:"-" json:"tags"`
+	Posts []Post  `bson:"-" json:"posts"`
+	Media []Media `bson:"-" json:"media"`
 }
 
-// DBCollection returns the name of mongodb collection.
+// DBCollection returns the name of mongodb collection
 func (Collection) DBCollection() string {
 	return "collection"
 }
 
+// Tag defines the tag of the User, Post and Collection
 type Tag struct {
 	ID    primitive.ObjectID `bson:"_id,omitempty" json:"id,string"`
-	Alias []string           `bson:",omitempty" json:"alias,omitempty"`
+	Alias []string           `bson:"alias,omitempty" json:"alias,omitempty"`
 }
 
-// DBCollection returns the name of mongodb collection.
+// DBCollection returns the name of mongodb collection
 func (Tag) DBCollection() string {
 	return "tags"
 }
 
-// type Comment struct {
-// 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,string"`
-// 	AuthorID primitive.ObjectID `json:"-"`
-// 	Text     string             `json:"text"`
-
-// 	Author User `bson:"-" json:"author,omitempty"`
-// }
-
+// Media defines the assets of Post
 type Media struct {
-	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id,string"`
-	MIME   string             `bson:",omitempty" json:"mime"`
-	Colors []Color            `bson:",omitempty" json:"colors"`
-	Size   int                `bson:",omitempty" json:"size"`
-	URL    string             `bson:",omitempty" json:"-"`
-	Path   string             `bson:",omitempty" json:"-"`
+	ID       primitive.ObjectID     `bson:"_id,omitempty" json:"id,string"`
+	MIME     string                 `bson:"mime,omitempty" json:"mime"`
+	Colors   []Color                `bson:"colors,omitempty" json:"colors"`
+	Size     int                    `bson:"size,omitempty" json:"size"`
+	URL      string                 `bson:"url,omitempty" json:"-"`
+	Path     string                 `bson:"path,omitempty" json:"-"`
+	Extended map[string]interface{} `bson:"extended,omitempty" json:"extended"`
 }
 
+// DBCollection returns the name of mongodb collection
 func (Media) DBCollection() string {
 	return "media"
 }
 
+// Color defines the color of Media
 type Color struct {
 	R, G, B uint8
 }
 
+// Page defines the page of Post and User
 type Page struct {
-	Markdown string               `bson:",omitempty" json:"markdown"`
-	MediaIDs []primitive.ObjectID `bson:",omitempty" json:"-"`
-	Media    []Media              `bson:"-" json:"media"`
+	HTML     string                 `bson:"html,omitempty" json:"html"`
+	MediaIDs []primitive.ObjectID   `bson:"mediaIDs,omitempty" json:"-"`
+	Media    []Media                `bson:"-" json:"media"`
+	Extended map[string]interface{} `bson:"extended,omitempty" json:"extended"`
 }
--- model/pixiv.go
+++ model/pixiv.go
@@ -2,36 +2,52 @@ package model
 
 // PixivUser extends User with Pixiv's user struct
 type PixivUser struct {
-	Name                 string `json:"name"`
-	Account              string `json:"account"`
-	IsFollowed           bool   `json:"isFollowed"`
-	IsPremium            bool   `json:"isPremium"`
-	TotalFollowing       int    `json:"totalFollowing"`
-	TotalPublicBookmarks int    `json:"totalPublicBookmarks"`
-	TotalIllusts         int    `json:"totalIllusts"`
-	TotalManga           int    `json:"totalManga"`
-	TotalNovels          int    `json:"totalNovels"`
-	TotalIllustSeries    int    `json:"totalIllustSeries"`
-	TotalNovelSeries     int    `json:"totalNovelSeries"`
+	IsFollowed           bool `bson:"isFollowed" json:"isFollowed"`
+	TotalFollowing       int  `bson:"totalFollowing" json:"totalFollowing"`
+	TotalPublicBookmarks int  `bson:"totalPublicBookmarks" json:"totalPublicBookmarks"`
+	TotalIllusts         int  `bson:"totalIllusts" json:"totalIllusts"`
+	TotalManga           int  `bson:"totalManga" json:"totalManga"`
+	TotalNovels          int  `bson:"totalNovels" json:"totalNovels"`
+	TotalIllustSeries    int  `bson:"totalIllustSeries" json:"totalIllustSeries"`
+	TotalNovelSeries     int  `bson:"totalNovelSeries" json:"totalNovelSeries"`
+}
 
-	Birth          string            `bson:",omitempty" json:"birth,omitempty"`
-	Country        string            `bson:",omitempty" json:"country,omitempty"`
-	Gender         string            `bson:",omitempty" json:"gender,omitempty"`
-	TwitterAccount string            `bson:",omitempty" json:"twitterAccount,omitempty"`
-	WebPage        string            `bson:",omitempty" json:"webPage,omitempty"`
-	Workspace      map[string]string `bson:",omitempty" json:"workspace,omitempty"`
+// PixivUserProfile extends UserProfile with Pixiv's user struct
+type PixivUserProfile struct {
+	Account        string            `bson:"account" json:"account"`
+	Name           string            `bson:"name" json:"name"`
+	IsPremium      bool              `bson:"isPremium" json:"isPremium"`
+	Birth          string            `bson:"birth,omitempty" json:"birth,omitempty"`
+	Country        string            `bson:"country,omitempty" json:"country,omitempty"`
+	Gender         string            `bson:"gender,omitempty" json:"gender,omitempty"`
+	TwitterAccount string            `bson:"twitterAccount,omitempty" json:"twitterAccount,omitempty"`
+	WebPage        string            `bson:"webPage,omitempty" json:"webPage,omitempty"`
+	Workspace      map[string]string `bson:"workspace,omitempty" json:"workspace,omitempty"`
 }
 
 // PixivIllust extends PostDetail with Pixiv's illust struct
 type PixivIllust struct {
-	Likes          int   `json:"likes"`
-	IsBookmarked   bool  `json:"isBookmarked"`
-	TotalBookmarks int   `json:"totalBookmarks"`
-	TotalView      int   `json:"totalView"`
-	UgoiraDelay    []int `json:"ugoiraDelay,omitempty"`
-	// TODO
-	Tools []string
+	IsBookmarked   bool `bson:"isBookmarked" json:"isBookmarked"`
+	TotalBookmarks int  `bson:"totalBookmarks" json:"totalBookmarks"`
+	TotalViews     int  `bson:"totalView" json:"totalView"`
 }
 
-type PixivNovel struct {
+// PixivIllustDetail extends PostDetail with Pixiv's illust struct
+type PixivIllustDetail struct {
+	// UgoiraDelay []int  `bson:"ugoiraDelay,omitempty" json:"ugoiraDelay,omitempty"`
+	Type string `bson:"type,omitempty" json:"type"`
+	// TODO
+	Tools []string `bson:"tools,omitempty" json:"tools,omitempty"`
+}
+
+// PixivNovelDetail extends PostDetail with Pixiv's novel struct
+type PixivNovelDetail struct {
+	IsBookmarked   bool `bson:"isBookmarked" json:"isBookmarked"`
+	TotalBookmarks int  `bson:"totalBookmarks" json:"totalBookmarks"`
+	TotalViews     int  `bson:"totalView" json:"totalView"`
+}
+
+// PixivMedia extends Media with extra info of Pixiv images
+type PixivMedia struct {
+	UgoiraDelay []int `bson:"ugoiraDelay,omitempty" json:"ugoiraDelay,omitempty"`
 }
